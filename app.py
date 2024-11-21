e6ee4f84cebad57ee85e069a3cb0776a4e6dec8f
daniyal-ahmad-khan <22100215@lums.edu.pk>
2024-11-21 16:03:18 +0500

    show matched image

--- app.py
+++ app.py
@@ -124,6 +124,7 @@ def verify_face_bytes():
                 TableName='face_collection',  
                 Key={'RekognitionId': {'S': response['FaceMatches'][0]['Face']['FaceId']}}
                 )
+            print(face)
             return jsonify({"message": "Face matched", "face_id": face["Item"]["Fullname"]["S"]}), 200
         else:
             return jsonify({"message": "No matching face found"}), 404
